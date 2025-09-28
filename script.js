5be7a1a675f0531505c273464f9a38c799fc0f9c
Anuj <anuj89806@gmail.com>
2025-09-29 01:40:53 +0530

    compeleted timer sectin

--- script.js
+++ script.js
@@ -18,26 +18,59 @@ let modes = {
 let currentMode = modes.work;
 
 let timerInterval = null;
+let minutes;
+let seconds;
+let totalSeconds;
+
+function formatTime(mins, secs) {
+  return `${mins < 10 ? "0" : ""}${mins}:${secs < 10 ? "0" : ""}${secs}`;
+}
 
 function switchTimerModes(duration, modeType, clickedBtn) {
+  clearInterval(timerInterval);
   currentDuration = duration;
-  timerDisplay.innerHTML = `${currentDuration}:00`;
+  totalSeconds = currentDuration * 60;
+  timerDisplay.innerHTML = formatTime(currentDuration, 0);
   currentMode = modeType;
   document.querySelectorAll(".timerModes button").forEach((btn) => {
     btn.classList.remove("shadow-lg", "shadow-blue-500/50");
+    btn.disabled = false;
   });
 
   clickedBtn.classList.add("shadow-lg", "shadow-blue-500/50");
+  clickedBtn.disabled = true;
 }
 function startTimer(mins) {
+  if (timerInterval) return;
+  if (totalSeconds === undefined || totalSeconds <= 0) {
+    totalSeconds = mins * 60;
+  }
+
   clearInterval(timerInterval);
 
   timerInterval = setInterval(() => {
-    mins++;
+    if (totalSeconds > 0) {
+      totalSeconds--;
+      minutes = Math.floor(totalSeconds / 60);
+      seconds = totalSeconds % 60;
+      timerDisplay.innerHTML = formatTime(minutes, seconds);
+    } else {
+      clearInterval(timerInterval);
+      timerInterval = null;
+      timerDisplay.innerHTML = `00:00`;
+    }
   }, 1000);
 }
-function pauseTimer() {}
-function resetTimer() {}
+function pauseTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+}
+function resetTimer() {
+  clearInterval(timerInterval);
+  totalSeconds = currentDuration * 60;
+  timerInterval = null;
+  timerDisplay.innerHTML = formatTime(currentDuration, 0);
+}
 
 timerModes.addEventListener("click", (e) => {
   if (e.target.classList.contains("workBtn")) {
@@ -54,7 +87,7 @@ timerModes.addEventListener("click", (e) => {
 });
 
 startBtn.addEventListener("click", () => {
-  startTimer();
+  startTimer(currentDuration);
 });
 
 pauseBtn.addEventListener("click", () => {
